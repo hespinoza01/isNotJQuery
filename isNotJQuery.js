87716f5e1c4c253c20ff261527d9073c4779a3c4
Harold Espinoza <haroldesptru@gmail.com>
2019-01-15 21:44:51 -0600

    Replanteamiento de la función constructora.

--- isNotJQuery.js
+++ isNotJQuery.js
@@ -1,15 +1,43 @@
-const $ = function(arg){
-  // Get the element(s)
-  let elements = Array.from(document.querySelectorAll(arg));
+function $ (selector){
+  // Verify the instance status
+  if(!this instanceof $){ return new $(selector); }
 
-  // If no element is found return and break the code
-  if(elements.length === 0){ return undefined; }
+  selector = selector || document.body; // Set default value for selector
+  let _$ = (Object.is(selector, document)) // Verify is selector isn't document
+              ? Object.create(_toDocument.prototype)
+              : Object.create($.prototype);
 
-  // If elements length === 1 convert the array to unique value in target
-  this.target = (elements.length === 1) ? elements[0] : elements;
+  // If using css selectors
+  if(typeof selector === "string"){
+    let elements = Array.from(document.querySelectorAll(selector));
 
-  // Is the object is not instance of $ return the node element
-  if(this instanceof $ === false){ return this.target; }
+    _$.target = (elements.length === 1) ? elements[0] : elements;
+  }
+
+  return _$;
+}
+
+$.prototype.text = function(value){
+  if(value === undefined) return;
+
+  this.target.textContent = value;
+  return this;
 };
 
-let data = $("#title");
+
+
+function _toDocument() {}
+_toDocument.prototype.ready = function(callback){
+  function complete(){
+    document.removeEventListener('DOMContentLoaded', callback);
+  }
+
+  document.addEventListener('DOMContentLoaded', function(){
+    callback();
+    complete();
+  });
+};
+
+$(document).ready(function() {
+  $('#title').text('Hello, world');
+});
